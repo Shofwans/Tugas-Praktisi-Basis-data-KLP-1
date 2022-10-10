c7254402e9f85a1d37e82a5900cb3cc51ffa669d
Shofwans <72340199+Shofwans@users.noreply.github.com>
2022-10-10 20:46:47 +0800

    Update DCL.sql

--- DCL.sql
+++ DCL.sql
@@ -1,25 +1,17 @@
 -- Membuat user baru
 CREATE USER 'DosenSisfor'@'localhost' IDENTIFIED BY '12345';
 
+CREATE USER 'Mahasiswa'@'localhost' IDENTIFED BY '67890';
+
 -- Memberi hak akses ke user 
 GRANT ALL PRIVILEGES 
 ON db_praktisiSisfo
 TO 'DosenSisfor'@'localhost';
 
-# Memberi akses SELECT kepada semua user
+# Memberi akses SELECT kepada Mahasiswa
 GRANT SELECT
 ON db_praktisiSisfo
-TO '*'@'localhost';
-
--- Menghapus akses tertentu (DROP) kepada semua user
-REVOKE DELETE
-ON db_praktisiSisfo
-FROM '*'@'localhost';
-
-# Menghapus hak akses user
-REVOKE INSERT
-ON db_praktisiSisfo
-FROM 'DosenSisfor'@'localhost';
+TO 'Mahasiswa'@'localhost';
 
 # Cek status hak akses user
 SHOW GRANTS FOR 'DosenSisfor'@'localhost';
